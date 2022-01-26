1e4d301acbaa3f36a4b2a3283686b54324d24fd6
bernard-collins <88315523+bernard-collins@users.noreply.github.com>
2022-01-26 11:05:34 +0000

    Add files via upload

---
+++ MyBank/MyBank/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Manfred", 100000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+
+            account.MakeWithdrawl(420, DateTime.Now, "Console");
+            account.MakeWithdrawl(1000, DateTime.Now, "For Me");
+
+            Console.WriteLine(account.GetAccountHistory());
+
+            // Test for a negative balance
+            //try
+            //{
+            //    var invalidAccount = new BankAccount("For Prince Manfred", -1000);
+            //}
+            //catch (ArgumentOutOfRangeException e)
+            //{
+            //    Console.WriteLine("Exception caught creating wwith negative balance");
+            //    Console.WriteLine(e.ToString());
+            //}
+        }
+    }
+}
