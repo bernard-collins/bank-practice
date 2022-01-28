3f78b4cd7a1186a3ad782158e512fe59018fa68c
bernard-collins <bernard.collins@bookingonline.co.uk>
2022-01-28 17:35:32 +0000

    feat: Finishing View Models

--- Vidly/Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Vidly/Controllers/MoviesController.cs
@@ -4,21 +4,51 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        //GET: Movies
+        
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-            return View(movie);
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1"},
+                new Customer { Name = "Customer 2"}
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers,
+            };
+
+
+
+            return View(viewModel);
         }
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
-        }
+
+        //These are not advised when Passing data to Views
+        //ViewData["Movie"] = movie;
+        //ViewBag.Movie = movie;
+
+        //var viewResult = new ViewResult();
+        //viewResult.ViewData.Model;
+
+        //public ActionResult Random()
+        //{
+        //    var movie = new Movie() { Name = "Shrek!" };
+        //    return View(movie);
+        //}
+
+        //[Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        //public ActionResult ByReleaseDate(int year, int month)
+        //{
+        //    return Content(year + "/" + month);
+        //}
 
 
         //public ActionResult Edit(int id)
